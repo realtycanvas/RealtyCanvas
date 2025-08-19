11eb030cb3045e8d189a0a4e212c8a01aac6c67d
Sudhanshu Srivastava <digitallynext2017@gmail.com>
2025-08-19 21:08:49 +0530

    Link added

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -5,12 +5,12 @@ export default function NotFound() {
       <p className="text-gray-600 dark:text-gray-400 mb-8 text-center max-w-md">
         The page you&apos;re looking for doesn&apos;t exist or has been removed.
       </p>
-      <a
+      <Link
         href="/"
         className="px-4 py-2 bg-brand-primary hover:bg-primary-600 text-white rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-brand-primary"
       >
         Return Home
-      </a>
+      </Link>
     </div>
   );
 }
